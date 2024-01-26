76bbcfe2825eac4f4b445fc8f627775211ca16e4
Efrem YOUSSOUF <youssouf.efrem@gmail.com>
2024-01-26 13:01:28 +0100

    Controller Crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,11 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Room;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -30,19 +33,48 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-         return $this->render('user/dashboard.html.twig');
+        return $this->render('user/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Bnb');
+            ->setTitle('<img src="/images/icon.svg" width="50">')
+            ->setFaviconPath('/images/icon.svg')
+            ->renderContentMaximized()
+            ->setLocales([
+                'en' => '🇬🇧 English',
+                'pl' => '🇵🇱 Polski'
+            ]);
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Roomsl', 'fas fa-list', Room::class);
-        //yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Assets');
+        yield MenuItem::linkToCrud('Locations', 'fas fa-cubes', Room::class);
+        yield MenuItem::section('Utilisateur');
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-users', User::class);
+        yield MenuItem::linkToRoute('Back to app', 'fas fa-arrow-left', 'app_room');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getFullname())
+            // use this method if you don't want to display the name of the user
+            ->displayUserName(false)
+
+            // you can return an URL with the avatar image
+            ->setAvatarUrl('/uploads/users/'. $user->getImage())
+            
+            ->addMenuItems([
+                MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+            ]);
     }
 }
--- src/Controller/Admin/ReviewCrudController.php
+++ src/Controller/Admin/ReviewCrudController.php
@@ -2,27 +2,30 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Room;
+use App\Entity\Review;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
-class RoomCrudController extends AbstractCrudController
+class ReviewCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Room::class;
+        return Review::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->onlyOnIndex(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextEditorField::new('comment'),
+            AssociationField::new('rooms')->hideOnIndex(),
+            AssociationField::new('traveler')->hideOnIndex(),
         ];
     }
-    */
+    
 }
